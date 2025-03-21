161108f833206eaffcce72caeaca21840f5e5c26
David-G-Cotom <brandoncotom202231460@cunoc.edu.gt>
2025-03-21 00:55:46 -0600

    Agregando la Carpeta del Proyecto creado por CLion

---
+++ _EDD_Proyecto1_PS25/cmake-build-debug/CMakeFiles/_EDD_Proyecto1_PS25.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/_EDD_Proyecto1_PS25.dir/main.cpp.o"
+  "CMakeFiles/_EDD_Proyecto1_PS25.dir/main.cpp.o.d"
+  "_EDD_Proyecto1_PS25"
+  "_EDD_Proyecto1_PS25.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/_EDD_Proyecto1_PS25.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ _EDD_Proyecto1_PS25/cmake-build-debug/CMakeFiles/_EDD_Proyecto1_PS25.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for _EDD_Proyecto1_PS25.
+# This may be replaced when dependencies are built.
